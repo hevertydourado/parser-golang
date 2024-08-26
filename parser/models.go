f74746b8532243c091d7d0d0d58c32902b3c2e63
hevertydourado <hevertydouradob@gmail.com>
2024-08-26 22:30:17 +0000

    Add comments to models.go

--- parser/models.go
+++ parser/models.go
@@ -1,8 +1,9 @@
 package parser
 
+// Match represents the data structure for a single game match.
 type Match struct {
-	TotalKills   int
-	Players      map[string]bool
-	Kills        map[string]int
-	KillsByMeans map[string]int
+	TotalKills   int               // Total number of kills that occurred in the match
+	Players      map[string]bool   // Set of players who participated in the match
+	Kills        map[string]int    // Map of player names to the number of kills they achieved
+	KillsByMeans map[string]int    // Map of kill means (e.g., weapons used) to the number of kills associated with each
 }
